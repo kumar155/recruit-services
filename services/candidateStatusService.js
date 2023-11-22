74ada7cea1a044d7e2c36bed3a84b13d61392e2c
kumar155 <skumar.donthu@gmail.com>
2023-11-22 22:18:28 +0530

    latest

--- services/candidateStatusService.js
+++ services/candidateStatusService.js
@@ -13,14 +13,14 @@ async function setStatus(obj) {
     let query;
     const status = await getStatus({ userId: obj.userId, jobId: obj.jobId });
     if (status !== null) {
-        query = `UPDATE candidateStatus SET
+        query = `UPDATE candidatestatus SET
         type = '${obj.type}',
         comments='${obj.comments}',
         created='${formatted()}',
         updated='${obj.updated}'
         WHERE userId = '${obj.userId}' and jobId='${obj.jobId}'`;
     } else {
-        query = `INSERT INTO candidateStatus
+        query = `INSERT INTO candidatestatus
         (userId, jobId, type, comments, created, updated)
         VALUES
         ('${obj.userId}', '${obj.jobId}', '${obj.type}', 'none', '${formatted()}', '${obj.updated}');`
@@ -35,7 +35,7 @@ async function setStatus(obj) {
 
 async function getStatus({ userId, jobId }) {
     const rows = await dbCon.execute(connection,
-        `SELECT * from candidateStatus where userId = '${userId}' and jobId='${jobId}';`
+        `SELECT * from candidatestatus where userId = '${userId}' and jobId='${jobId}';`
     );
     const data = helper.emptyOrRows(rows);
     return data[0] ? data[0] : null;
