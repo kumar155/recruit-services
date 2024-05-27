67536c8f8d532b6f2a839c79d22f8d8746baf69a
kumar155 <skumar.donthu@gmail.com>
2024-05-27 20:31:24 +0530

    server changes

--- connection.js
+++ connection.js
@@ -3,12 +3,12 @@ const mysql = require("mysql2");
 function connection() {
     try {
         const pool = mysql.createPool({
-            // host: '3.149.240.114',
-            // port: '3306',
-            // user: 'satish',
-            // password: 'Satish@123',
-            // database: 'recruit',
-            ...localConnection,
+            host: '3.149.240.114',
+            port: '3306',
+            user: 'satish',
+            password: 'Satish@123',
+            database: 'recruit',
+            // ...localConnection,
             timezone: 'utc',
             connectionLimit: 10,
             waitForConnections: true,
