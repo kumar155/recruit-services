eed2fb3cdf7c54e7d62c121eeffdd5575db30997
kumar155 <skumar.donthu@gmail.com>
2024-06-01 23:02:52 +0530

    more logs

--- transactions/candidateJob.trans.js
+++ transactions/candidateJob.trans.js
@@ -1,9 +1,11 @@
+const logger = require("../config/logger-config");
 const dbCon = require("../connection");
 
 const connection = async () => await dbCon.connection();
 
 async function insertResumeAnalysis(body, MLresponse) {
     const candidateJobId = body.candidateJobId;
+    logger.info('response of the resume AI analysis: %s', MLresponse);
     const query = `UPDATE candidatejob SET 
      matchScore='${MLresponse['JD Match']}',
      missingKeyWords='${MLresponse['Missing Keywords'].join(',')}',
@@ -11,10 +13,12 @@ async function insertResumeAnalysis(body, MLresponse) {
      WHERE (candidateJobId='${candidateJobId}' AND id <> 0)`;
 
     const result = await dbCon.execute(connection, query);
+    logger.info('query response of sql: %s', result);
     let message = 'error occured while analysing the resume';
     if (result.affectedRows) {
         message = 'Uploaded and resume metrics generated successfully';
     }
+    logger.info('query affected rows: %s', result);
 
     return { message };
 }
