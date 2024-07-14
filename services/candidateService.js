a2b8baba666b71ee310656775419691e1b36a967
kumar155 <skumar.donthu@gmail.com>
2024-07-14 19:09:43 +0530

    latest

--- services/candidateService.js
+++ services/candidateService.js
@@ -233,44 +233,10 @@ async function insertCandidateJob(user) {
 
     return { message, next: 1, randomString };
 }
-
-async function update(id, programmingLanguage) {
-    const result = await dbCon.execute(connection,
-        `UPDATE programming_languages 
-    SET name="${programmingLanguage.name}", released_year=${programmingLanguage.released_year}, githut_rank=${programmingLanguage.githut_rank}, 
-    pypl_rank=${programmingLanguage.pypl_rank}, tiobe_rank=${programmingLanguage.tiobe_rank} 
-    WHERE id=${id}`
-    );
-
-    let message = "Error in updating programming language";
-
-    if (result.affectedRows) {
-        message = "Programming language updated successfully";
-    }
-
-    return { message };
-}
-
-async function remove(id) {
-    const result = await dbCon.execute(connection,
-        `DELETE FROM programming_languages WHERE id=${id}`
-    );
-
-    let message = "Error in deleting programming language";
-
-    if (result.affectedRows) {
-        message = "Programming language deleted successfully";
-    }
-
-    return { message };
-}
-
 module.exports = {
     getAll,
     createStep1,
     createStep2,
-    update,
-    remove,
     getSelection,
     apply,
     getHistory,
